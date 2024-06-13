e81cafe3e04dcaae0229e544c86657ccc50121d0
duum <hyperion2011@hotmail.com>
2024-06-13 16:47:53 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+
+namespace Backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace Backend
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment CurrentEnvironment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            CurrentEnvironment = env;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvc();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddCors();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            var urlAceptadas = Configuration.GetSection("AllowedHosts").Value!.Split(",");
+            app.UseCors(builder =>
+                builder.WithOrigins(urlAceptadas)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
